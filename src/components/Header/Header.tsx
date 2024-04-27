116a6887aab5e72e3ea73ee7764dff29fdf67810
Vladislav <rolanddoor@gmail.com>
2024-04-27 15:03:50 +0300

    little fix for Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,58 +8,60 @@ import styles from "./Header.module.scss";
 
 export const Header: FC = () => {
   return (
-    <header className={styles["app-header"]}>
-      <Image
-        className={styles["app-header__logo"]}
-        src="/logo.png"
-        alt="logo"
-        width={274}
-        height={40}
-      />
+    <>
+      <header className={styles["app-header"]}>
+        <Image
+          className={styles["app-header__logo"]}
+          src="/logo.png"
+          alt="logo"
+          width={274}
+          height={40}
+        />
 
-      <nav className={styles["navbar"]}>
-        <ul className={styles["navbar__list"]}>
-          {links.map((link) => (
-            <Link
-              href={link.hash}
-              className={styles["navbar__list-item"]}
-              key={link.name}
+        <nav className={styles["navbar"]}>
+          <ul className={styles["navbar__list"]}>
+            {links.map((link) => (
+              <Link
+                href={link.hash}
+                className={styles["navbar__list-item"]}
+                key={link.name}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </ul>
+
+          <a href="#" className="btn-link">
+            Participate
+          </a>
+
+          <div className={styles["navbar__vertical-line"]}></div>
+
+          <div className={styles["navbar__social-media"]}>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              {link.name}
-            </Link>
-          ))}
-        </ul>
-
-        <a href="#" className="btn-link">
-          Participate
-        </a>
-
-        <div className={styles["navbar__vertical-line"]}></div>
-
-        <div className={styles["navbar__social-media"]}>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaFacebook />
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaTwitter />
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram />
-          </a>
-        </div>
-      </nav>
-    </header>
+              <FaFacebook />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
